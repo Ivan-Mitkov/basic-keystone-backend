1fde1c6b7e897aa4747f78fa085577dc497fe78d
Ivan Mitkov <ivan.mitkov@gmail.com>
2021-03-16 13:04:13 +0200

    add signin

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
 const { Keystone } = require("@keystonejs/keystone");
 const { GraphQLApp } = require("@keystonejs/app-graphql");
 const { AdminUIApp } = require("@keystonejs/app-admin-ui");
+const { PasswordAuthStrategy } = require("@keystonejs/auth-password");
 const { MongooseAdapter: Adapter } = require("@keystonejs/adapter-mongoose");
+
 const dotenv = require("dotenv").config();
 const PROJECT_NAME = "Keystone Blog";
 const adapterConfig = { mongoUri: process.env.MONGO_URI };
 
-const PostSchema=require('./lists/Post')
-const UserSchema=require('./lists/User')
+const PostSchema = require("./lists/Post");
+const UserSchema = require("./lists/User");
 
 const keystone = new Keystone({
   adapter: new Adapter(adapterConfig),
-  cookieSecret:process.env.COOKIE_SECRET
+  cookieSecret: process.env.COOKIE_SECRET,
 });
 
-keystone.createList('Post',PostSchema)
-keystone.createList('User',UserSchema)
+keystone.createList("Post", PostSchema);
+keystone.createList("User", UserSchema);
+//password under the lists
+const authStrategy = keystone.createAuthStrategy({
+  type: PasswordAuthStrategy,
+  list: "User",
+  config: {
+    identityField: "email",
+    secretField: "password",
+  },
+});
 
 module.exports = {
   keystone,
   apps: [
     new GraphQLApp(),
-    new AdminUIApp({ name: PROJECT_NAME, enableDefaultRoute: true }),
+    new AdminUIApp({ name: PROJECT_NAME, enableDefaultRoute: true,authStrategy }),
   ],
 };
